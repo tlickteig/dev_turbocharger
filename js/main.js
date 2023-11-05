6519884a37ef908e61bdcff5b66d5c5e3b986cae
tlickteig <timmattmelnat@gmail.com>
2023-11-04 19:42:53 -0500

    Got automatic dark mode working

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    /*const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+    if (darkThemeMq.matches) {
+        $("html").attr("data-bs-theme", "dark");
+    } else {
+        $("html").attr("data-bs-theme", "light");
+    }*/
+});
