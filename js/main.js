fa07ca3e922de7c3cefd01e4eb3c6423f7be189a
tlickteig <timmattmelnat@gmail.com>
2023-11-06 19:16:23 -0600

    Moved css and js out of files to avoid 404 issues

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    /*const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
-    if (darkThemeMq.matches) {
-        $("html").attr("data-bs-theme", "dark");
-    } else {
-        $("html").attr("data-bs-theme", "light");
-    }*/
-});
